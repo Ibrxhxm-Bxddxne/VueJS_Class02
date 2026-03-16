affc17f6c127befedebe5c39a9cce510d3ce2fd1
unknown <brahem.bad662@gmail.com>
2026-03-16 15:26:43 +0000

    V2

--- projet01/src/App.vue
+++ projet01/src/App.vue
@@ -1,7 +1,17 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <div>
+    <h1>Welcome to Vue 3</h1>
+    <NewComponent />
+  </div>
 </template>
+
+<script>
+import NewComponent from "./components/NewComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    NewComponent
+  }
+};
+</script>
---
+++ projet01/src/components/NewComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p>{{ message }}</p>
+    <button @click="showAlert()">Click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NewComponent",
+
+  data() {
+    return {
+      message: "Hello from New Component!"
+    };
+  },
+
+  methods: {
+    showAlert() {
+      alert(this.message);
+    }
+  }
+};
+</script>
+
+<style scoped>
+button {
+  padding: 10px;
+  background-color: #42b983;
+  color: white;
+  border: none;
+}
+</style>
